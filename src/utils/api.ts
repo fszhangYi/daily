d98095f1c69fa371a6ae05ba85065b2b3a80afb9
Qi <92540319+fszhangYi@users.noreply.github.com>
2023-11-25 12:25:52 +0800

    add one artical

---
+++ src/utils/api.ts
@@ -0,0 +1,35 @@
+import OpenAI from "openai";
+
+const apiKey = process.env.REACT_APP_WEATHER_KEY;
+const city = process.env.REACT_APP_WEATHER_CITY;
+const openaiKey = process.env.REACT_APP_OPENAI_KEY;
+const openaiBase = process.env.REACT_APP_OPENAI_BASE;
+
+const getWeather = async () => {
+  // 使用fetch发送请求
+  const {result:{daily_fcsts}} = await fetch(`https://gfapi.mlogcn.com/weather/v001/day?areacode=${city}&days=1&key=${apiKey}&output_type=json`).then(d=>d.json())
+  const rst = daily_fcsts[0];
+  return rst;
+}
+
+const formatContent = async (content: any) => {
+  const openai = new OpenAI({
+    apiKey: openaiKey,
+    baseURL: openaiBase,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: `
+    请将下面的日报内容格式化成markdown格式之后返回,返回格式应该是英文，并且不要有任何多余的文字：
+    ${content}
+    ` }],
+    model: "gpt-4",
+  });
+  return completion.choices[0] as unknown as string;
+}
+
+export {
+  getWeather,
+  formatContent,
+}
